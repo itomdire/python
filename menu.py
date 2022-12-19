c3a627d8acee18a7d57c0b7722be3defa297f248
itomdire <motidire@gmail.com>
2022-12-19 10:44:42 -0800

    (initial commit)

---
+++ menu.py
@@ -0,0 +1,14 @@
+print(f"**SELECT THE MENU YOU WANT**")
+print(f"1- MENU A ")
+print(f"2- MENU B ")
+print(f"3- MENU C ")
+
+menu = input(f"Please enter your Choice :")
+if menu == "1":
+    print(f"Your Choice is MENU A ")
+elif menu == "2":
+    print(f"Your Choice is Menu B")
+elif menu == "3":
+    print(f"Your Choice is Menu C")
+else:
+    print(f"You entered incorrect input!")
